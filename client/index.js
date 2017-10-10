1e0e2c39ebde9ea3fb1d6feb4e4fe65ecdfe3ded
9404368350 <mahesh.katre47@gmail.com>
2017-10-11 00:46:35 +0530

    final commit

--- client/index.js
+++ client/index.js
@@ -1,19 +1,19 @@
 var seneca = require ("seneca")();
 
 
-seneca.client({host:"127.0.0.1", port:8085}).act({"role":"list","cmd":"read"}, function (err,response) {
-	if (err) return console.log (err.msg);
-	console.log(response)
-})
-
-
-// seneca.client({host:"127.0.0.1", port:8085}).act({"role":"add","cmd":"write"}, function (err,response) {
+// seneca.client({host:"127.0.0.1", port:8085}).act({"role":"list","cmd":"read"}, function (err,response) {
 // 	if (err) return console.log (err.msg);
-// 	console.log (response);
+// 	console.log(response)
 // })
 
 
-seneca.client({host:"127.0.0.1", port:8085}).act({"role":"incident","cmd":"get","id":1}, function (err,response) {
+seneca.client({host:"127.0.0.1", port:8085}).act({"role":"add","cmd":"write"}, function (err,response) {
 	if (err) return console.log (err.msg);
 	console.log (response);
 })
+
+
+// seneca.client({host:"127.0.0.1", port:8085}).act({"role":"incident","cmd":"get","id":"SNI123456"}, function (err,response) {
+// 	if (err) return console.log (err.msg);
+// 	console.log (response);
+// })
--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ var seneca = require('seneca')();
 var jsonfile = require ('jsonfile');
 var fs = require('fs');
 
-var data = require ("./MOCK_DATA.json")
+var data = require ("./incidents.json")
 
 // seneca.add("role:list,cmd:read", function(msg, done) {
    
    
-//     jsonfile.readFile ('./MOCK_DATA.json', function (err, obj){
+//     jsonfile.readFile ('./incidents.json', function (err, obj){
 //         done(null, {Incidents: obj});
 //      })
 
@@ -17,7 +17,7 @@ var data = require ("./MOCK_DATA.json")
 // {
 
 //     var obj = {"incidentId":"SNI654321","description":"service running"};
-//     jsonfile.writeFile("./MOCK_DATA.json", obj, {flag: 'a'}, function (err) {
+//     jsonfile.writeFile("./incidents.json", obj, {flag: 'a'}, function (err) {
 //     if (err){
 //         console.error(err);
 //     }
@@ -30,34 +30,34 @@ var data = require ("./MOCK_DATA.json")
 
 //append at the end
 
-// seneca.add("role:add,cmd:write", function(msg,done)
-// {
-// var obj = JSON.parse(fs.readFileSync('./MOCK_DATA.json', 'utf8'));
-// obj.push({"teamId":"4","status":"pending"});
-// //obj = JSON.stringify(obj);
-// jsonfile.writeFile("./MOCK_DATA.json", obj, function (err) {
-//     if (err){
-//         console.error(err);
-//     }
-//     else {
-//         var ans = true;
-//         done(null, {answer: ans})
-//     }
-// })
-// })
+seneca.add("role:add,cmd:write", function(msg,done)
+{
+var obj = JSON.parse(fs.readFileSync('./incidents.json', 'utf8'));
+obj.push({"incidentId":"SNI12876", "description":"downgraded service","status":"In progress"});
+//obj = JSON.stringify(obj);
+jsonfile.writeFile("./incidents.json", obj, function (err) {
+    if (err){
+        console.error(err);
+    }
+    else {
+        var ans = true;
+        done(null, {answer: ans})
+    }
+})
+})
 
 seneca.add("role:incident,cmd:get", function(msg, done) {
     
-        var user = data.filter(function(usr) { return usr.id == msg.id});
+        var inc = data.filter(function(insi) { return insi.incidentId == msg.id});
         var error =null;
-        if (user.length === 0 ) {
-            error = Error ("user not found");
+        if (inc.length === 0 ) {
+            error = Error ("incident not found");
     
         }
         else{
-            user = user[0]
+            inc = inc[0]
         }
         
-        done(error, {"user" : user})
+        done(error, {"incident" : inc})
     })
 seneca.listen(8085);
